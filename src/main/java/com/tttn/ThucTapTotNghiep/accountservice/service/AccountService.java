14646d894a1d9f9c47cd107055f3b9e357f2711a
Thanh Dat <daothanhdat2604@gmail.com>
2024-04-21 17:19:34 +0700

    new

--- src/main/java/com/tttn/ThucTapTotNghiep/accountservice/service/AccountService.java
+++ src/main/java/com/tttn/ThucTapTotNghiep/accountservice/service/AccountService.java
@@ -1,16 +1,46 @@
 package com.tttn.ThucTapTotNghiep.accountservice.service;
 
 import com.tttn.ThucTapTotNghiep.accountservice.model.Account;
+import com.tttn.ThucTapTotNghiep.accountservice.repository.AccountRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
 import java.util.List;
+@AllArgsConstructor
+@Transactional
+@Service
+public class AccountService {
+    AccountRepository accountRepository;
 
 
-public interface AccountService {
-    List<Account> findAll();
+    public List<Account> findAll() {
+        return accountRepository.findAll();
+    }
 
-//    Optional<Account> findById(Integer integer);
-    Account save(Account ac);
-    void deleteById(Integer integer);
-    Account updateAc(Integer id , Account ac);
 
+
+    public Account save(Account ac) {
+        return accountRepository.save(ac);
+    }
+
+
+    public void deleteById(Integer id) {
+        accountRepository.deleteById(id);
+    }
+
+
+    public Account updateAc(Integer id, Account account){
+        Account ac = accountRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Khong co account voi id:"+id));
+        ac.setUser_password(account.getUser_password());
+        ac.setUser_email(account.getUser_email());
+        ac.setUser_type(account.getUser_type());
+        ac.setPhone_number(account.getPhone_number());
+        ac.setFull_name(account.getFull_name());
+        accountRepository.save(ac);
+        return ac;
+    }
 
 }
--- src/main/java/com/tttn/ThucTapTotNghiep/subjectclassservice/service/SubjectClassService.java
+++ src/main/java/com/tttn/ThucTapTotNghiep/subjectclassservice/service/SubjectClassService.java
@@ -1,15 +1,57 @@
 package com.tttn.ThucTapTotNghiep.subjectclassservice.service;
 
+
+
 import com.tttn.ThucTapTotNghiep.subjectclassservice.model.SubjectClass;
+import com.tttn.ThucTapTotNghiep.subjectclassservice.repository.SubjectClassReponsitory;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.List;
+import java.util.Optional;
 
-public interface SubjectClassService {
+@AllArgsConstructor
+@Transactional
+@Service
+public class SubjectClassService  {
+    SubjectClassReponsitory subjectClassReponsitory;
 
-    List<SubjectClass> findAll();
 
-    SubjectClass save(SubjectClass sc);
 
-    void deleteById(Integer integer);
-    SubjectClass updateSc(Integer id ,SubjectClass ac);
+    public Optional<SubjectClass> findById(Integer integer) {
+        return subjectClassReponsitory.findById(integer);
+    }
+
+    public List<SubjectClass> findAll() {
+        return subjectClassReponsitory.findAll();
+    }
+
+
+    public SubjectClass save(SubjectClass sc) {
+        Timestamp timestamp=new Timestamp(System.currentTimeMillis());
+        sc.setCreated_at(timestamp);
+        return subjectClassReponsitory.save(sc);
+    }
+
+    public void deleteById(Integer id) {
+        subjectClassReponsitory.deleteById(id);
+    }
+
+
+    public SubjectClass updateSc(Integer id, SubjectClass subjectClass){
+        SubjectClass sc = subjectClassReponsitory.findById(id).orElseThrow(() -> new EntityNotFoundException("Khong co lop hoc voi id:"+id));
+        sc.setSubject_name(subjectClass.getSubject_name());
+        sc.setCreated_by(subjectClass.getCreated_by());
+        sc.setCreated_at(subjectClass.getCreated_at());
+        sc.setSchool_year(subjectClass.getSchool_year());
+        sc.setNumber_of_group(subjectClass.getNumber_of_group());
+        sc.setMember_per_group(subjectClass.getMember_per_group());
+        sc.setGroup_register_method(subjectClass.getGroup_register_method());
+        subjectClassReponsitory.save(sc);
+        return sc;
+    }
+
 }

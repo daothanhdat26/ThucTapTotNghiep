f32ac4c02a3d9ce4c75e1a2cbf58fc55d5a2f82d
khanghoang782 <jakekgg2002@gmail.com>
2024-04-23 08:47:47 +0700

    add-groupService

--- src/main/java/com/tttn/ThucTapTotNghiep/groupService/controller/GroupController.java
+++ src/main/java/com/tttn/ThucTapTotNghiep/groupService/controller/GroupController.java
@@ -5,6 +5,7 @@ import com.tttn.ThucTapTotNghiep.groupService.service.GroupService;
 import com.tttn.ThucTapTotNghiep.groupService.wrapper.GroupInfo;
 import com.tttn.ThucTapTotNghiep.groupService.wrapper.MemberInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,17 +17,24 @@ public class GroupController {
     @Autowired
     GroupService groupService;
 
+    //tạo nhóm bằng danh sách
     @PostMapping("/api/class/create-groups")
     public ResponseEntity<String>createGroupFromList(@RequestBody List<GroupInfo> groupInfoList){
         return groupService.createGroupForClass(groupInfoList);
     }
+    //thêm thành viên theo danh sách
     @PostMapping("/api/class/add-member")
     public ResponseEntity<String>addMemberIntoGroup(@RequestBody List<MemberInfo> memberList){
         return groupService.addGroupMemberFromList(memberList);
     }
+    //cập nhật leader
     @PutMapping("/api/class/{classId}/group/{groupId}/set-leader/{userId}")
     public ResponseEntity<String>updateGroupLeader(@PathVariable Integer classId,@PathVariable Integer groupId,@PathVariable Integer userId){
         return groupService.updateGroupLeader(classId,groupId,userId);
+    }
+    @PostMapping("api/class/create-a-group")
+    public ResponseEntity<String>createSingleGroup(@RequestBody GroupInfo groupInfo){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
